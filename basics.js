797bc0e597de71ae45b6ce2f9feb149f45fa4427
Luke Sorrenti <lrsorrenti@hotmail.com>
2022-10-13 16:43:46 -0500

    finished section on type annotation, and how TS will underline problems in our IDE

---
+++ basics.js
@@ -0,0 +1,5 @@
+var pi = 3.14159;
+var testin = 23;
+console.log("HELLO");
+console.log("GOODBYE!!!");
+console.log(Math.round(7.2341241245524));
---
+++ basics.ts
@@ -0,0 +1,5 @@
+let pi: number = 3.14159
+let testin: number = 23
+console.log("HELLO")
+console.log("GOODBYE!!!")
+console.log(Math.round(7.2341241245524))
