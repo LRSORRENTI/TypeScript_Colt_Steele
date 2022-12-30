d6904402f450894a39ae4fc3bfc4d1cef0b57ba7
Luke Sorrenti <lrsorrenti@hotmail.com>
2022-12-30 10:00:00 -0600

    added js file using tsc command

---
+++ basics/december2022/basics.js
@@ -0,0 +1,3 @@
+console.log("hello there");
+console.log('general kenobi');
+console.log(Math.round(3.14159265359));
--- basics/december2022/basics.ts
+++ basics/december2022/basics.ts
@@ -1,3 +1,4 @@
+let favoriteGame: string = 'Dark Souls 3'
 console.log(`hello there`)
 console.log('general kenobi')
 console.log(Math.round(3.14159265359))
