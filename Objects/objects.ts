cb3f0fe33bacd50d22dc0a19c34a073748d4740d
Luke Sorrenti <lrsorrenti@hotmail.com>
2022-12-31 10:45:01 -0600

    finished excess properties section, and finished quiz, added quiz in txt file for future reference

--- Objects/objects.ts
+++ Objects/objects.ts
@@ -9,3 +9,20 @@ function printDogName(dog: {name: string, breed: string}): void {
 }
 
 printDogName({name: 'rey', breed: 'german shepherd'})
+
+let coordinate: {x: number, y: number} = {x: 34, y: 2}
+
+function makeRandCoordinate(param1:number, param2: number){
+    console.log(param1 * param2);
+    return param1 * param2;
+}
+
+makeRandCoordinate(234, 2)
+
+function add(sum: {num1: number, num2: number}): void {
+    console.log(sum.num1 + sum.num2)
+}
+
+add({num1: 5, num2: 8})
+
+printDogName({name: 'molly', breed: 'rottweiler'})
