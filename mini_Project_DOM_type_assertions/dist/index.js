89dd1f01f2f8fe22b2fb5bafdfc8920b50a97b24
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-01-31 14:18:33 -0600

    added intro section to DOM manip with TS

--- mini_Project_DOM_type_assertions/dist/index.js
+++ mini_Project_DOM_type_assertions/dist/index.js
@@ -5,3 +5,5 @@ console.log('watch!!!');
 console.log('auto update in js?');
 console.log('browswe sync updating?');
 console.log("live server?");
+const btn = document.getElementById('btn');
+console.log(btn);
--- mini_Project_DOM_type_assertions/src/index.ts
+++ mini_Project_DOM_type_assertions/src/index.ts
@@ -4,3 +4,7 @@ console.log('watch!!!')
 console.log('auto update in js?')
 console.log('browswe sync updating?')
 console.log("live server?")
+
+const btn = document.getElementById('btn')
+
+console.log(btn)
