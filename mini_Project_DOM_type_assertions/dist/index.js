50cf1885226b18015eda7d49e8fe748b23402a19
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-02-01 08:44:20 -0600

    added intro to section on non null assertion, one way is to add a ? after variable is saved to getElement or ClassName etc

--- mini_Project_DOM_type_assertions/dist/index.js
+++ mini_Project_DOM_type_assertions/dist/index.js
@@ -1,9 +1,12 @@
 "use strict";
-console.log('hi there!!!!');
-console.log('changed');
-console.log('watch!!!');
-console.log('auto update in js?');
-console.log('browswe sync updating?');
-console.log("live server?");
+// console.log('hi there!!!!')
+// console.log('changed')
+// console.log('watch!!!')
+// console.log('auto update in js?')
+// console.log('browswe sync updating?')
+// console.log("live server?")
 const btn = document.getElementById('btn');
-console.log(btn);
+// console.log(btn)
+btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", function () {
+    alert("clicked");
+});
--- mini_Project_DOM_type_assertions/src/index.ts
+++ mini_Project_DOM_type_assertions/src/index.ts
@@ -1,10 +1,14 @@
-console.log('hi there!!!!')
-console.log('changed')
-console.log('watch!!!')
-console.log('auto update in js?')
-console.log('browswe sync updating?')
-console.log("live server?")
+// console.log('hi there!!!!')
+// console.log('changed')
+// console.log('watch!!!')
+// console.log('auto update in js?')
+// console.log('browswe sync updating?')
+// console.log("live server?")
 
 const btn = document.getElementById('btn')
 
-console.log(btn)
+// console.log(btn)
+
+btn?.addEventListener("click", function(){
+    alert("clicked")
+})
