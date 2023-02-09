874c9b0e829a4a9d27ff8075a94e1d475488ac34
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-02-09 09:16:24 -0600

    Added intro to JS classes refresher, class keyword, new keyword, methods inside of the class, most of this is recap for me

--- JSClassesRefresher/dist/index.js
+++ JSClassesRefresher/dist/index.js
@@ -1,4 +1,22 @@
-"use strict";
+
 console.log("working?");
 console.log(12);
 console.log("liteserver is updating now? ");
+
+class Food {
+    taste(){
+      console.log("mmmm tasty")  
+    }
+    bleh(){
+        console.log("bleh! not tasty ")
+    }
+}
+
+const foodOne = new Food()
+foodOne.taste()
+foodOne.bleh()
+console.log(foodOne)
+
+const foodTwo = new Food()
+
+foodTwo.bleh()
