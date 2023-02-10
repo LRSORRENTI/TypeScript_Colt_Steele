942ce5973afe0998dc9c7842dcd4bcd06ea15529
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-02-10 09:31:55 -0600

    added more to JS classes recap, inside of constructor funcs, we use the 'this' keyword to assign individual instantions when utilizing the new keyword

--- JSClassesRefresher/dist/index.js
+++ JSClassesRefresher/dist/index.js
@@ -4,15 +4,23 @@ console.log(12);
 console.log("liteserver is updating now? ");
 
 class Player {
+  constructor(arg1, arg2 ){
+   this.first = arg1;
+   this.last = arg2;
+  }
   taunt(){
     console.log("taunt")
   }
 }
 
-const player1 = new Player()
+const player1 = new Player("Luke", "Sorrenti")
 
 player1.taunt()
 
-const player2 = new Player()
+const player2 = new Player("Frank", "Sorrenti")
 
 player2.taunt()
+
+console.log(player1, player2)
+console.log(player1.first, player1.last, 
+  player2.first, player2.last)
