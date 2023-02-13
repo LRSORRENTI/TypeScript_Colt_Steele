cddbfbc41e14b3bc8ea08a0f55b32d4ba1c61956
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-02-13 09:03:34 -0600

    by default, every value, method, class is public, but explicitly annotating a value with public can help with code clarity

---
+++ TypeScriptClasses/src/publicModifier.ts
@@ -0,0 +1,33 @@
+// In this section we'll learn about 
+// additonal TS modifiers, first is public
+
+// By default in TS and JS every single 
+// property , class, or method is public
+
+//
+
+
+
+class Animal {
+  public species: string;
+  public numLegs: number;
+    constructor( species: string, 
+        numLegs: number){
+        this.species = species;
+        this.numLegs = numLegs;
+    }
+}
+
+const dog = new Animal("Canine", 4)
+dog.species
+
+// Remember, the pubic modifier is auto-set 
+// on everything, but if it makes your code 
+// more readable, and understandable, use 
+// it prefixed to a given value like above
+
+// If a value is public, it means it can 
+// be accessed and is available anywhere
+
+// Explicit 'public' prefix annotation 
+// is typically used for code clarity
