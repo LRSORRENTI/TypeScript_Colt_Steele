2dc8f34a43a159d7cd2f0de1c99b977c0af46223
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-02-13 08:53:57 -0600

    added section on readonly modifiers

--- TypeScriptClasses/dist/readonly.js
+++ TypeScriptClasses/dist/readonly.js
@@ -1,6 +1,7 @@
 "use strict";
 // We've seen readonly before in 
-// the context of objects
+// the context of objects, but we 
+// can use them in classes
 class Person {
     constructor(name, occu, age) {
         this.name = name;
@@ -9,3 +10,13 @@ class Person {
     }
 }
 const luke = new Person("Luke", "WebDevHopefully", 29);
+luke.name = "LUKE";
+// trying to change luke.name won't work since 
+// we made name readonly in the class definition
+luke.occupation = "Full Stack Web Developer";
+// but we can change any other property
+// This can be very useful, and can make it clear 
+// that a given value SHOULD only ever 
+// be read, not written to, in your code, 
+// also makes it easier for other devs to see 
+// what you're values should be
--- TypeScriptClasses/src/readonly.ts
+++ TypeScriptClasses/src/readonly.ts
@@ -1,10 +1,11 @@
 
 // We've seen readonly before in 
-// the context of objects
+// the context of objects, but we 
+// can use them in classes
 
 
 class Person {
-    name: string;
+   readonly name: string;
     occupation: string;
     age: number;
 
@@ -18,3 +19,18 @@ class Person {
 
 const luke = new Person("Luke", "WebDevHopefully",
 29)
+
+luke.name = "LUKE"
+
+// trying to change luke.name won't work since 
+// we made name readonly in the class definition
+
+luke.occupation = "Full Stack Web Developer"
+
+// but we can change any other property
+
+// This can be very useful, and can make it clear 
+// that a given value SHOULD only ever 
+// be read, not written to, in your code, 
+// also makes it easier for other devs to see 
+// what you're values should be
