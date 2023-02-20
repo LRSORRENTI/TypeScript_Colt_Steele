3c8f62799b21b453ccd82690981ac3b27fdcd603
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-02-20 15:25:02 -0600

    ok I renamed my folders for better organization and git yelled at me, oops, too many changes

--- mini_Project_DOM_type_assertions/src/typeAssertions.ts
+++ mini_Project_DOM_type_assertions/src/typeAssertions.ts
@@ -1,1 +1,20 @@
 let mystery = "hello world"
+
+// the above TS infers mystery is a string, but we 
+// can override this with: 
+
+let newMystery: unknown = "hi"
+
+const numChars = (newMystery as string).length
+
+// newMystery is still type unknown, but with the 
+// as keyword, we tell TS, in this specific 
+// instance treat newMystery as if it is a string
+
+// But if newMystery is a num, or any other data type, 
+// problems happen. This example doesn't seem to be the 
+// best but it's important to know the concept itself, 
+// type assertions tell TS to treat something AS a 
+// certain type
+
+const numberOfChars = mystery.length
