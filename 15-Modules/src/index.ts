dc9203d0be00fbb743c7e33a7ea6c446c24d2841
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-02-28 13:59:56 -0600

    added section on import / export keywords, to use a function or code elsewhere, use the export keyword prefixed, then import that to the desired file with the import {funcName} from ./filename.js important to note it needs to end in .js, .ts will not work

--- 15-Modules/src/index.ts
+++ 15-Modules/src/index.ts
@@ -1,1 +1,15 @@
+// So now we used the keyword export in the 
+// utilityExample.ts file, and we use the 
+// 'import' keyword to bring those in to use 
+// inside of index.ts, note that the file 
+// we import from can't have a .ts file, it 
+// must be .js
+
+import {add, sample} from './utilityExample.js'
+
 console.log("hello universe")
+
+add(2, 4)
+
+sample(['s'])
+
