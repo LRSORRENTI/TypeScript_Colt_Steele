ecb6bed8ab9a29bbfd9d2f9c6074f0a2c03bf277
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-03-04 16:40:19 -0600

    having some problems, going to try adding lite-server dependency

--- 16-WebpackAndTS/webpack.config.js
+++ 16-WebpackAndTS/webpack.config.js
@@ -71,7 +71,7 @@ module.exports = {
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
-        publicPath: "/dist",
+        publicPath: "auto",
     },
   
 };
