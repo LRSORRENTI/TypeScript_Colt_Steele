afc50e1af4b83a31383704227fa3c4ba90e59ee9
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-03-15 18:36:50 -0500

    added section on props, using interfaces to structure props, then destructure them so that we wouldn't need to do props.name or props.age, and just pass in {name} or {age}

--- 17-React_And_TypeScript/to-do-list-app/src/components/Greeter.tsx
+++ 17-React_And_TypeScript/to-do-list-app/src/components/Greeter.tsx
@@ -1,7 +1,38 @@
 import React from "react"; 
+// in the function below, our 
+// argument list could become very long 
+// if we need to have many different arguments 
+// to be passed in 
 
-function Greeter(): JSX.Element {
-    return <h1>Hello there!</h1>
+// so we can make an interface for the props
+
+
+// function Greeter(props: {person: string}): JSX.Element {
+//     return <h1>Hello there {props.person}!</h1>
+// }
+
+// export default Greeter;
+
+// so we can make an interface for the props
+
+interface GreeterProps  { 
+ person: string;
+ age: number;
 }
+
+// function Greeter(props: GreeterProps): JSX.Element {
+//     return <h1>Hello there {props.person}! You are 
+//     {props.age} years old</h1>
+// }
+
+// But something commonly done is to destructure from 
+// the props
+
+
+function Greeter({person, age}: GreeterProps): JSX.Element {
+    return <h1>Hello there {person}! You are 
+    {age} years old</h1>
+}
+
 
 export default Greeter;
